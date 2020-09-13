994f212fa305e8f2565eeab3c33000a5cb99a1df
tryproject2016 <kumarprashant.iitg.6032@gmail.com>
2020-09-13 17:49:52 +0530

    first commit

---
+++ coding_blocks/snake.js
@@ -0,0 +1,51 @@
+//     function init (){
+//         var canvas = document.getElementById("mycanvas");
+//         W = H = canvas.width = canvas.height = 1000;
+//         pen = canvas.getContext("2d");
+//         cs = 50;
+//         snkae = {
+//             init_len: 5,
+//             color:"blue",
+//             cells:[],
+//             direction:"right",
+
+
+//             createSnake:function (){
+//                 for(var i=this.init_len;i>=0;i--){
+//                     this.cells.push({x:i,y:0});
+//                 }
+//             },
+//             drawSnake:function(){
+//                 pen.fillRect(this.cells[i].x,this.cells[i].y,cs,cs);
+//             }
+//         };
+//     }
+
+//     function draw(){
+//         snkae.drawSnake();
+//     }
+
+//     function update(){
+
+//     }
+
+// function gameloop (){
+
+
+// }
+canvas = document.getElementById("mycanvas");
+
+// canvas.width = 100;
+// canvas.height = 100;
+
+// canvas is used to draw graphics
+
+pen = canvas.getContext("2d");
+
+
+
+
+
+
+
+
